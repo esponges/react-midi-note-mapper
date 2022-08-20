b94c2feed1307823173bbbbcc5557766ec3309e2
esponges <esponges@gmail.com>
2022-08-20 17:02:20 -0500

    use Note type

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import "./App.css";
 
 import { midiToBlob } from "./utils/midi";
 
+type Note = Track['notes'][number] | null;
+
 function App() {
   const audioRef = React.useRef<HTMLAudioElement>(null);
+  const playinRef = React.useRef<boolean>(false);
   const timeRef = React.useRef<number>(0);
 
   const [midi, setMidi] = React.useState<Track>();
-  const [note, setNote] = React.useState<string|null>(null);
+  const [note, setNote] = React.useState<Note>(null);
+  const [noteId, setNoteId] = React.useState<number|null>(null);
 
   useEffect(() => {
     const getMidi = async () => {
@@ -20,16 +24,48 @@ function App() {
     getMidi();
   }, []);
 
-  console.log(midi);
-  const handleMapNote = (note: string) => {
+  const handleMapNote = (note: Note) => {
     setNote(note);
   }
+  
+  useEffect(() => {
+    let currentTimeout: ReturnType<typeof setTimeout>;
+    
+    if (playinRef.current && midi) {
+      const nextNoteIdx = midi.notes.findIndex(n => n.velocity === noteId) + 1;
+      const nextNote = midi.notes[nextNoteIdx];
+
+      if (nextNote) {
+        const nextTime = nextNote.time - timeRef.current;
+        currentTimeout = setTimeout(() => {
+          handleMapNote(nextNote);
+        }, nextTime);
+      }
+    }
+
+    return () => {
+      if (currentTimeout) {
+        clearTimeout(currentTimeout);
+      }
+    }
+  } , [midi, noteId]);
+
+  // useEffect(() => {
+  //   let currentTimeout: ReturnType<typeof setTimeout>;
+
+  //   if (isPlaying && midi) {
+  //     const nextNoteIdx = midi.notes.findIndex(n => n.velocity === noteId) + 1;
+  //     const nextNote = midi.notes[nextNoteIdx];
+
+
 
   const handleAudioPlay = () => {
     if (audioRef.current && !!midi) {
       audioRef.current.play();
       // map the very first note in the state
-      setNote(midi.notes[0].name);
+      // velocity is the closest we have to an unique id
+      playinRef.current = true;
+      handleMapNote(midi.notes[0]);
     }
   }
 
@@ -44,7 +80,8 @@ function App() {
     if (audioRef.current && !!midi) {
       audioRef.current.pause();
       audioRef.current.currentTime = 0;
-      setNote(null);
+      handleMapNote(null);
+      playinRef.current = false;
     }
   }
 
@@ -53,7 +90,7 @@ function App() {
       <header className="App-header">
         <h5>Current Note</h5>
         <p>
-          {note}
+          {note?.name}
         </p>
         <div>
           <button onClick={handleAudioPlay}>Play</button>
